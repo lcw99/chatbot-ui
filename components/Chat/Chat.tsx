44f076ffc44d6e22469855f323afc656e220cd40
chang <changlee99@gmail.com>
2023-04-27 12:12:39 +0900

    connect to my llm endpoint

--- components/Chat/Chat.tsx
+++ components/Chat/Chat.tsx
@@ -411,18 +411,6 @@ export const Chat = memo(({ stopConversationRef }: Props) => {
 
                   {models.length > 0 && (
                     <div className="flex h-full flex-col space-y-4 rounded-lg border border-neutral-200 p-4 dark:border-neutral-600">
-                      <ModelSelect />
-
-                      <SystemPrompt
-                        conversation={selectedConversation}
-                        prompts={prompts}
-                        onChangePrompt={(prompt) =>
-                          handleUpdateConversation(selectedConversation, {
-                            key: 'prompt',
-                            value: prompt,
-                          })
-                        }
-                      />
 
                       <TemperatureSlider
                         label={t('Temperature')}
@@ -440,7 +428,7 @@ export const Chat = memo(({ stopConversationRef }: Props) => {
             ) : (
               <>
                 <div className="sticky top-0 z-10 flex justify-center border border-b-neutral-300 bg-neutral-100 py-2 text-sm text-neutral-500 dark:border-none dark:bg-[#444654] dark:text-neutral-200">
-                  {t('Model')}: {selectedConversation?.model.name} | {t('Temp')}
+                  {t('Temp')}
                   : {selectedConversation?.temperature} |
                   <button
                     className="ml-2 cursor-pointer hover:opacity-50"
@@ -455,13 +443,6 @@ export const Chat = memo(({ stopConversationRef }: Props) => {
                     <IconClearAll size={18} />
                   </button>
                 </div>
-                {showSettings && (
-                  <div className="flex flex-col space-y-10 md:mx-auto md:max-w-xl md:gap-6 md:py-3 md:pt-6 lg:max-w-2xl lg:px-0 xl:max-w-3xl">
-                    <div className="flex h-full flex-col space-y-4 border-b border-neutral-200 p-4 dark:border-neutral-600 md:rounded-lg md:border">
-                      <ModelSelect />
-                    </div>
-                  </div>
-                )}
 
                 {selectedConversation?.messages.map((message, index) => (
                   <MemoizedChatMessage
--- utils/server/index.ts
+++ utils/server/index.ts
@@ -30,10 +30,26 @@ export const OpenAIStream = async (
   key: string,
   messages: Message[],
 ) => {
+  let basaran = true;
+
   let url = `${OPENAI_API_HOST}/v1/chat/completions`;
+  if (basaran) {
+    url = `${OPENAI_API_HOST}/v1/completions`;
+  } 
   if (OPENAI_API_TYPE === 'azure') {
     url = `${OPENAI_API_HOST}/openai/deployments/${AZURE_DEPLOYMENT_ID}/chat/completions?api-version=${OPENAI_API_VERSION}`;
   }
+  console.log(messages)
+  let prompt = "";
+  for (const m of messages) {
+    if (m.role === 'user') {
+      prompt += "B:" + m.content.trim() + "\nA:";
+    } else {
+      prompt += m.content + "\n";
+    }
+  }
+  console.log(prompt);
+  console.log("temperature=" + temperature);
   const res = await fetch(url, {
     headers: {
       'Content-Type': 'application/json',
@@ -50,16 +66,25 @@ export const OpenAIStream = async (
     method: 'POST',
     body: JSON.stringify({
       ...(OPENAI_API_TYPE === 'openai' && {model: model.id}),
-      messages: [
-        {
-          role: 'system',
-          content: systemPrompt,
-        },
-        ...messages,
-      ],
-      max_tokens: 1000,
-      temperature: temperature,
-      stream: true,
+      ...(basaran !== true && { 
+        messages: [
+          {
+            role: 'system',
+            content: systemPrompt,
+          },
+          ...messages,
+        ],
+        max_tokens: 1000,
+        temperature: temperature,
+        stream: true,
+      }),
+      ...(basaran && {
+        prompt: prompt,
+        max_tokens: 2000,
+        temperature: temperature,
+        top_p: 0.7,
+        stream: true,
+      }),
     }),
   });
 
@@ -85,29 +110,67 @@ export const OpenAIStream = async (
   }
 
   const stream = new ReadableStream({
-    async start(controller) {
-      const onParse = (event: ParsedEvent | ReconnectInterval) => {
-        if (event.type === 'event') {
-          const data = event.data;
+      async start(controller) {
+        let stopped = false;
+        let no_gen_count = 0;
+        let gen_concat = "";
+        const onParse = (event: ParsedEvent | ReconnectInterval) => {
+        if (!basaran) {
+          if (event.type === 'event') {
+            const data = event.data;
 
-          try {
-            const json = JSON.parse(data);
-            if (json.choices[0].finish_reason != null) {
-              controller.close();
-              return;
+            try {
+              const json = JSON.parse(data);
+              if (json.choices[0].finish_reason != null) {
+                controller.close();
+                return;
+              }
+              const text = json.choices[0].delta.content;
+              const queue = encoder.encode(text);
+              controller.enqueue(queue);
+            } catch (e) {
+              controller.error(e);
             }
-            const text = json.choices[0].delta.content;
-            const queue = encoder.encode(text);
-            controller.enqueue(queue);
-          } catch (e) {
-            controller.error(e);
+          }
+        } else {
+          if (event.type === 'event') {
+            const data = event.data;
+
+            try {
+              const json = JSON.parse(data);
+              let text = json.choices[0].text;
+              if (text.length == 0) {
+                no_gen_count += 1;
+                console.log("no gen = " + no_gen_count)
+                if (no_gen_count > 5) {
+                  controller.close();
+                  stopped = true;
+                }
+                return;
+              }
+              // console.log("[" + text + "]");
+              gen_concat += text;
+              if (gen_concat.indexOf("\nB") >= 0) {
+                  controller.close();
+                  stopped = true;
+                  return;
+              }
+              no_gen_count = 0;
+              const queue = encoder.encode(text);
+              controller.enqueue(queue);
+            } catch (e) {
+              controller.error(e);
+            }
           }
         }
       };
 
       const parser = createParser(onParse);
-
       for await (const chunk of res.body as any) {
+        if (stopped) {
+          stopped = false;
+          break;
+        }
         parser.feed(decoder.decode(chunk));
       }
     },
